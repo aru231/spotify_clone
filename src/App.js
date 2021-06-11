53a2ad1b3544801b53d948b73f2ed9541f7f40cc
Arihant Jain <jainarihant231@gmail.com>
2021-06-11 19:48:19 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
-
-import './App.css';
+import "./App.css";
 import Bar from "./components/Bar";
 import Login from "./Login";
+import React, { useEffect, useState } from "react";
+import { getTokenfromUrl } from "./spotify";
 
 function App() {
+  const [token, setToken] = useState(null);
+
+  useEffect(() => {
+    const hash = getTokenfromUrl();
+    window.location.hash = "";
+    const _token = hash.access_token;
+    if (_token) {
+      setToken(_token);
+    }
+    // console.log("token",token);
+  }, []);
   return (
     <div className="App">
-      <Login/>
-      <Bar/>
+      {token ? <h1>logged</h1> : <Login />}
     </div>
   );
 }
--- src/spotify.js
+++ src/spotify.js
@@ -11,17 +11,16 @@ const scopes = [
   "user-modify-playback-state",
 ];
 
-// export const getTokenFromResponse = () => {
-//   return window.location.hash
-//     .substring(1)
-//     .split("&")
-//     .reduce((initial, item) => {
-//       var parts = item.split("=");
-//       initial[parts[0]] = decodeURIComponent(parts[1]);
-
-//       return initial;
-//     }, {});
-// };
+export const getTokenfromUrl=()=>{
+  return window.location.hash
+  .substring(1)
+  .split('&')
+  .reduce((initial,item)=>{
+    let parts= item.split('=');
+    initial[parts[0]]= decodeURIComponent(parts[1]);
+    return initial;
+  },{});
+}
 
 export const loginUrl = `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join(
   "%20"
